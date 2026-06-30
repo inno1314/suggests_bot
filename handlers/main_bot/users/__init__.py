a602132d6ce83f0d75a9d7b437ecd96e9de74956
inno1314 <cubeu8gt4@gmail.com>
2026-06-30 21:30:07 +0300

    Fix Router conflict: register status handler on main bot users router directly

--- handlers/main_bot/users/__init__.py
+++ handlers/main_bot/users/__init__.py
@@ -11,6 +11,10 @@ from .back import router as back_router
 from .settings import router as setts_router
 from .get_subscription import router as get_sub
 from .ok_button import router as ok_router
+from aiogram.filters import ChatMemberUpdatedFilter, MEMBER, KICKED
+from aiogram.types import ChatMemberUpdated
+from sqlalchemy.ext.asyncio import AsyncSession
+from data.config import db
 
 router = Router(name=__name__)
 
@@ -24,3 +28,13 @@ router.include_routers(start_router,
                        get_sub,
                        ok_router)
 
+
+
+@router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=KICKED))
+async def mark_as_inactive(event: ChatMemberUpdated, session: AsyncSession):
+    await db.bot_api.change_user_status(session, event.from_user.id, new_status=False)
+
+
+@router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=MEMBER))
+async def mark_as_active(event: ChatMemberUpdated, session: AsyncSession):
+    await db.bot_api.change_user_status(session, event.from_user.id, new_status=True)
